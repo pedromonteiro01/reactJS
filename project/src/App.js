6b1128f67b65d611848cd3f716a10c41c6a9e3b3
Pedro <pmapm@ua.pt>
2021-10-05 11:22:10 +0100

    adding links and navigation

--- project/src/App.js
+++ project/src/App.js
@@ -3,6 +3,7 @@ import {Route, Switch} from 'react-router-dom'
 import AllMeetupsPage from './pages/AllMeetups';
 import NewMeetupPage from './pages/NewMeetup';
 import FavoritesPage from './pages/Favorites';
+import MainNavigation from './components/layout/MainNavigation';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
     // with <Switch> only one component can be rendered
     // with exact prop /favorites will present FavoritesPage instead of AllMeetupsPage
     <div>
+      <MainNavigation />
       <Switch>
         <Route path='/' exact={true}>
           <AllMeetupsPage />  
