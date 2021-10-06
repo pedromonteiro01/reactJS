72e9c3c6f414f4d200c141d1be7a38c334798468
Pedro <pmapm@ua.pt>
2021-10-06 19:31:18 +0100

    sending a post request (using firebase)

--- project/src/pages/NewMeetup.js
+++ project/src/pages/NewMeetup.js
@@ -2,7 +2,16 @@ import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() { 
     function addMeetupHandler(meetupData) { 
-
+        fetch(
+            'https://react-getting-started-757a0-default-rtdb.firebaseio.com/meetups.json',
+            {
+                method:'POST',
+                body:JSON.stringify(meetupData),
+                headers:{
+                    'Content-Type':'application/json'
+                }
+            }
+        ); //js function -> sends http request, by default sends GET request
     }
 
     return (
