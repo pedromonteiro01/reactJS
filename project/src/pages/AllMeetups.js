134ddfe13e9cb0830cb60c7d1e9485c6825a9dfd
Pedro <pmapm@ua.pt>
2021-10-07 11:05:49 +0100

    useEffect hook (displaying data from firebase)

--- project/src/pages/AllMeetups.js
+++ project/src/pages/AllMeetups.js
@@ -1,19 +1,34 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import MeetupList from "../components/meetups/MeetupList";
 
 function AllMeetupsPage() { 
     const [isLoading, setIsLoading] = useState(true);
     const [loadedMeetups, setLoadedMeetups] = useState([]);
-    // default is a GET request
-    fetch(
-        'https://react-getting-started-757a0-default-rtdb.firebaseio.com/meetups.json'
-    ).then((response)=>{
-        return response.json();
-    }).then((data)=>{
-        setIsLoading(false); // set to false once we have the data
-        setLoadedMeetups(data);
-    }); 
+
+    useEffect(()=>{
+        setIsLoading(true);
+        // default is a GET request
+        fetch(
+            'https://react-getting-started-757a0-default-rtdb.firebaseio.com/meetups.json'
+        ).then((response)=>{
+            return response.json();
+        }).then((data)=>{
+            const meetups = [];
+
+            for (const key in data) {
+                const meetup = {
+                    id:key,
+                    ...data[key]
+                };
+
+                meetups.push(meetup);
+            }
+
+            setIsLoading(false); // set to false once we have the data
+            setLoadedMeetups(meetups);
+        }); 
+    }, []); // empty array because there are not external dependencies
 
     if (isLoading) {
         return <section>
